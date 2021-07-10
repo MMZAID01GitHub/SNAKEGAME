3672df756cb3286449484ba29cc0d3c0c0344313
damma <damma@Mohammad-Mohib-Zaidi-thinkpad>
2021-07-10 02:55:45 -0400

    Got new movement to work for as many pieces as I want!!!
    changed boxes to type Piece which is a class extending rectangle that
    has variables to hold previousX and previousY values

--- SnakeGame/src/Assets/Game.java
+++ SnakeGame/src/Assets/Game.java
@@ -18,7 +18,7 @@ public class Game extends Application {
 	final int WIDTH = 600;
 	final int HEIGHT = 400;
 
-	double size = 50;
+	double size = 30;
 	double rectX = 100;
 	double rectY = 200;  
 	double previousX = 0;
@@ -28,8 +28,7 @@ public class Game extends Application {
 	double xSpeed = size;
 	
 
-	ArrayList<Rectangle> snake = new ArrayList<Rectangle>();
-	ArrayList<Location> locations = new ArrayList<Location>();
+	ArrayList<Piece> snake = new ArrayList<Piece>(); //THE PIECES OF THE SNAKE
 	
 
 	Directions dir;
@@ -44,44 +43,36 @@ public class Game extends Application {
 
 
 
-		stage.setTitle("Basic JavaFX demo");
+		stage.setTitle("Snake");
 
 		Group root = new Group();
 		Scene scene = new Scene(root, WIDTH, HEIGHT);
 		scene.setFill(Color.BLACK);
-		scene.setOnKeyPressed(this::movement);
+		scene.setOnKeyPressed(this::movement);//ARROWKEY MOVEMENT HANDLER
 
 		dir = Directions.DOWN;
-		Rectangle rect = new Rectangle();
-		rect.setLayoutX(rectX);
-		rect.setLayoutY(rectY);
-		rect.setWidth(size);
-		rect.setHeight(size);
-		rect.setFill(Color.AZURE);
-		rect.setStroke(Color.ORANGE);
-		root.getChildren().add(rect);
-
-		Rectangle rect2 = new Rectangle();
-		rect2.setLayoutX(rectX);
-		rect2.setLayoutY(rectY);
-		rect2.setWidth(size);
-		rect2.setHeight(size);
-		rect2.setFill(Color.PURPLE);
-		rect2.setStroke(Color.ORANGE);
-		root.getChildren().add(rect2);
+//		Piece rect = new Piece();
+//		rect.setLayoutX(rectX);
+//		rect.setLayoutY(rectY);
+//		rect.setWidth(size);
+//		rect.setHeight(size);
+//		rect.setFill(Color.AZURE);
+//		rect.setStroke(Color.ORANGE);
+//		root.getChildren().add(rect);
+		for(int i = 0; i < 10; i ++) {//INSTANTIATING SOME PIECES
+			snake.add(new Piece());
+		}
+		for(Piece piece : snake) {
+			piece.setLayoutX(rectX);
+			piece.setLayoutY(rectY);
+			piece.setWidth(size);
+			piece.setHeight(size);
+			piece.setFill(Color.AQUAMARINE);
+			piece.setStroke(Color.ORANGE);
+			root.getChildren().add(piece);
+		}
 		
-		Rectangle rect3 = new Rectangle();
-		rect3.setLayoutX(rectX);
-		rect3.setLayoutY(rectY);
-		rect3.setWidth(size);
-		rect3.setHeight(size);
-		rect3.setFill(Color.BLUE);
-		rect3.setStroke(Color.ORANGE);
-		root.getChildren().add(rect3);
-		
-		
-
-
+		snake.get(0).setFill(Color.CORAL);//MAKING THE HEAD COLOR UNIQUE
 
 		
 
@@ -97,11 +88,14 @@ public class Game extends Application {
 				
 				if(currentTime-lastUpdate >= 250000000) {
 					// UPDATE
+
+					//RECORDING COORDINATES BEFORE REPLACEMENT
+					for(Piece p : snake) {
+						p.previousX = p.getLayoutX();
+						p.previousY = p.getLayoutY();
+					}
 					
-					previousX = rectX;
-					previousY = rectY;
-					previousPreviousX = rect2.getLayoutX();
-					previousPreviousY = rect2.getLayoutY();
+					//MOVING A SPACE IN DIRECTION OF DIR
 					switch(dir) {
 					case RIGHT:
 						rectX+=xSpeed;
@@ -117,35 +111,20 @@ public class Game extends Application {
 						break;
 					}
 					
-					rect.setLayoutX(rectX);
-					rect.setLayoutY(rectY);
-					rect2.setLayoutX(previousX);
-					rect2.setLayoutY(previousY);
-					rect3.setLayoutX(previousPreviousX);
-					rect3.setLayoutY(previousPreviousY);
-//					if(dir == Directions.DOWN) {
-	//
-//						rect2.setLayoutY(rect.getLayoutY()-size);
-//						rect2.setLayoutX(rectX);
-//						
-//						
-	//
-//					}
-//					if(dir == Directions.UP) {
-//						rect2.setLayoutY(rect.getLayoutY()+size);
-//						rect2.setLayoutX(rectX);
-//						
-//					}
-//					if(dir == Directions.LEFT) {
-//						rect2.setLayoutX(rect.getLayoutX()+size);
-//						rect2.setLayoutY(rectY);
-//						
-//					}
-//					if(dir == Directions.RIGHT) {
-//						rect2.setLayoutX(rect.getLayoutX()-size);
-//						rect2.setLayoutY(rectY);
-//					
-//					}
+					
+					//DRAWING THE HEAD
+					snake.get(0).setLayoutX(rectX);
+					snake.get(0).setLayoutY(rectY);
+					
+					//DRAWING REST OF PIECES IN THE PREVIOUS LOCATION OF THE PIECE PRECEDING IT
+					for(int i = 1; i < snake.size(); i++) {
+						snake.get(i).setLayoutX(snake.get(i-1).previousX);
+						snake.get(i).setLayoutY(snake.get(i-1).previousY);
+
+					}
+
+					
+					
 					lastUpdate = currentTime;
 				}
 				
@@ -163,7 +142,7 @@ public class Game extends Application {
 		DOWN
 	}
 
-	public void movement(KeyEvent event) {
+	public void movement(KeyEvent event) {//CHANGING DIRECTION WITH KEY PRESS
 		if(event.getCode() == KeyCode.RIGHT) {
 			dir=Directions.RIGHT;
 		}
---
+++ SnakeGame/src/Assets/Piece.java
@@ -0,0 +1,10 @@
+package Assets;
+
+import javafx.scene.shape.Rectangle;
+
+public class Piece extends Rectangle {
+	double previousX;
+	double previousY;
+	
+	
+}
